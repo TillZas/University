9766409764a74e5368f64411af39acb66586b0a5
Sergey Gulyaev <TillZas@gmail.com>
2017-10-16 22:31:18 +0300

    adds

--- Python/Faces.py
+++ Python/Faces.py
@@ -1,16 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from plainbox.impl.pod import POD
 from sklearn import neighbors, datasets
 
-iris = datasets.load_iris()
-X = iris.data[:,:2]
-y = iris.target
 
-a = [0,1,2,3,11,5,6,7,8,9,10]
 
-a = np.delete(a,3)
+a = [1,2,2,2,2,6,0,0,0,0,9,9,9]
+b = [0,0,0,1,1,1,2,2,2,2,5,5,5]
 
-print(a)
+print(np.copy(a))
+#print(np.argmax(np.bincount(b,a)))
+
+#iris = datasets.load_iris()
+#X = iris.data[:,:2]
+#y = iris.target
+
+#a = [0,1,2,3,11,5,6,7,8,9,10]
+
+#a = np.delete(a,3)
+
+#print(a)
 
 def ONN(X,y,u):
     """Classifies by the nearest training object
@@ -60,9 +69,52 @@ def LOOforKNN(X,y):
         kvarray.append(cd)
     return gk,kvarray
 
-gk,ga = LOOforKNN(X,y)
-print(gk)
+def ParsenWindow(X,y,K,h,u):
+    P = [K(((x[0]-u[0])**2 + (x[1]-u[1])**2)/h,1) for x in X]
+    return np.argmax(np.bincount(y,P))
 
-plt.plot([x for x in range(1,len(X))], ga, 'ro')
-plt.axis([0, len(X)+1, 0, ga[np.argmax(ga)]+1])
-plt.show()
+
+def PotentiaFunctions(X, y, K, h, t, u):
+    P = []
+    for i in range(0, len(X)):
+        P.append(t[i]*K(((X[i][0] - u[0]) ** 2 + (X[i][1] - u[1]) ** 2) / h[i], 1))
+    return np.argmax(np.bincount(y, P))
+
+
+def GetHForPotentialFunctions(X, y, p):
+    diff = 1
+    u = [[], 0]
+    t = [0 for x in X]
+    h = [1 for x in X]
+    while(diff>p):
+        cd = 0
+        for i in range(1, len(X)):
+            u[0] = X[i]
+            u[1] = y[i]
+            Xt = np.delete(X, i, 0)
+            yt = np.delete(y, i)
+            if (PotentiaFunctions(Xt, yt, K, h,t, u[0]) != u[1]):
+                cd = cd + 1
+                t[i] = t[i]+1
+            else:
+                print(str(PotentiaFunctions(Xt, yt, K, h,t, u[0]))+" "+str(u[1]))
+        diff = cd/len(X)
+        print(str(diff*10)+"%")
+
+    return h,t
+
+def K(x,l):
+    if(x>l):
+        return 0;
+    else:
+        return (l-x)/l;
+
+
+#h = GetHForPotentialFunctions(X,y,0.1)
+#print(h)
+#gk,ga = LOOforKNN(X,y)
+#print(gk)
+
+#plt.plot([x for x in range(1,len(X))], ga, 'ro')
+#plt.axis([0, len(X)+1, 0, ga[np.argmax(ga)]+1])
+#plt.show()
--- Python/MyCode.py
+++ Python/MyCode.py
@@ -53,6 +53,46 @@ def KNN(X,y,u,k):
 
 # In[41]:
 
+def PotentiaFunctions(X, y, K, h, t, u):
+    P = []
+    for i in range(0, len(X)):
+        P.append(t[y[i]]*K( ((X[i][0] - u[0])**2 + (X[i][1] - u[1])**2) / h[i], 1))
+    return np.argmax(np.bincount(y, P))
+
+
+def GetHForPotentialFunctions(X, y, p):
+    diff = 1
+    u = [[], 0]
+    t = []
+    for i in range(0,len(np.unique(y))):
+        t.append(0);
+    h = [0.45 for x in X]
+    stopsignal = 100
+    while(diff>p and stopsignal>=0):
+        cd = 0
+        for i in range(1, len(X)):
+            u[0] = X[i]
+            u[1] = y[i]
+            Xt = np.delete(X, i, 0)
+            yt = np.delete(y, i)
+            ht = np.delete(h, i)
+            if (PotentiaFunctions(Xt, yt, K, ht,t, u[0]) != u[1]):
+                cd = cd + 1
+                t[u[1]] = t[u[1]]+1
+        diff = cd/len(X)
+        print(str(diff*100)+"% :"+str(cd)+" errs of "+str(len(X)))
+        stopsignal = stopsignal - 1
+    return h,t
+
+def K(x,l):
+    if(x>l):
+        return 0;
+    else:
+        return (l-x)/l;
+
+
+
+
 
 iris = datasets.load_iris()
 X = iris.data[:,:2]
@@ -63,10 +103,14 @@ print(y)
 KNN(X,y,[1,1],4)
 
 
+hs,t = GetHForPotentialFunctions(X,y,0.02)
+print(hs)
+print(t)
+
 # In[51]:
 
 
-h = 0.02
+h = 0.04
 
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
@@ -74,7 +118,7 @@ y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
-Z = np.array([ONN(X,y,u) for u in  np.c_[xx.ravel(), yy.ravel()]])
+Z = np.array([PotentiaFunctions(X, y, K, hs, t, u) for u in  np.c_[xx.ravel(), yy.ravel()]])
 Z = Z.reshape(xx.shape)
 
 
@@ -86,5 +130,5 @@ plt.pcolormesh(xx, yy, Z, cmap=ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
 plt.scatter(X[:,0],X[:,1], c=y, cmap=ListedColormap(['#FF0000', '#00FF00', '#0000FF']))
 plt.xlim(xx.min(), xx.max())
 plt.ylim(yy.min(), yy.max())
-plt.title("Iris ONN")
+plt.title("Parsen window")
 plt.show()
