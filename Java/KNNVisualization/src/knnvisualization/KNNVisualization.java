134dd537650d2bf346dc798d3bd9e8e442af5e96
Sergey Gulyaev <TillZas@gmail.com>
2017-10-23 14:24:24 +0300

    JavaInit

---
+++ Java/KNNVisualization/src/knnvisualization/KNNVisualization.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knnvisualization;
+
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author tillz
+ */
+public class KNNVisualization extends JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
