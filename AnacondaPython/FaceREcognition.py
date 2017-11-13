830827031f419068bdd52a0e4b94609976eecbc1
Sergey Gulyaev <TillZas@gmail.com>
2017-11-13 23:17:34 +0300

    Webcam

--- AnacondaPython/FaceREcognition.py
+++ AnacondaPython/FaceREcognition.py
@@ -5,20 +5,31 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-img = cv2.imread('in.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cam = cv2.VideoCapture(0)
+while True:
+    rv,wcImage = cam.read()
+    #cv2.imshow('my webcam', wcImage)
 
-#faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-faces = face_cascade.detectMultiScale(gray, 1.211, 3)
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-print(len(faces))
-cv2.imshow('img',img)
-cv2.waitKey(0)
+    #img = cv2.imread('in.jpg')
+    img = cv2.resize(wcImage, (0,0), fx=1, fy=1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.211, 3)
+    #faces = face_cascade.detectMultiScale(gray, 1.2, 1)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    #print(len(faces))
+    #cv2.imshow('img',img)
+    cv2.imshow('img',cv2.flip(img,1) )
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 cv2.destroyAllWindows()
+cam.release()
